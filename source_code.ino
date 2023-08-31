ad795b7730861f66d7c070dfd6e991b366fbe5f4
Narayan Vyas <narayanvyas87@gmail.com>
2023-08-31 06:16:46 +0530

    Update source_code.ino

--- source_code.ino
+++ source_code.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x20,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
 
 void setup() {
   lcd.init();
